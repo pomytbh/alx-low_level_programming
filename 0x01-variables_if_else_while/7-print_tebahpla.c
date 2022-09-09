cfeb1273042657f1a9af263c57bf3590b1b21386
pomytbh <pomytbh@gmail.com>
2022-09-09 06:36:22 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+
+/**
+
+  *  Prints the alphabet at reverse
+
+  *
+
+  * 
+
+  */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'z'; c >= 'a'; c--)
+
+	{
+
+		putchar(c);
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
