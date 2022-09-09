10defe5668b305cd9489aecfd92b12a8b82cb07b
pomytbh <pomytbh@gmail.com>
2022-09-09 06:23:25 -0700

    alphabet dis

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Prints the alphabetic
+
+ *
+
+ * 
+
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+         {
+
+		putchar(c);}
+
+
+
+	putchar('\n');
+
+	return (0);
+
+}
