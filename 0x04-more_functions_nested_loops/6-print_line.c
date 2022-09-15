e3f22000a7c6bfe2c52647f3d83b72565af1589c
pomytbh <pomytbh@gmail.com>
2022-09-15 04:58:18 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_line - draws a straight line in the terminal
+
+ * @n: int
+
+ */
+
+
+
+void print_line(int n)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < n; i++)
+
+	{
+
+		_putchar('_');
+
+	}
+
+	_putchar('\n');
+
+}
