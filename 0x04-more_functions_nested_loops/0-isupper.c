87eae310ccf27792b1d1fae72c28d2ae26c28a19
pomytbh <pomytbh@gmail.com>
2022-09-15 12:37:50 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,22 @@
-
-
-[A[A[A[B[B
+#include "main.h"
+/**
+ *
+ *  * _isupper - checks for uppercase character.
+ *
+ *   * @c: char
+ *
+ *    * Return: 0
+ *
+ *     */
+int _isupper(int c)
+{
+	int i;
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
