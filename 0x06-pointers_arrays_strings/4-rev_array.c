76e952dbfa2474ffacc7abe6b350c5e0938703e3
pomytbh <pomytbh@gmail.com>
2022-09-21 10:41:05 -0700

    4-rev_array.c
    qs/wq
    fd.cv

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ *
+ * @a: []
+ * @n: sizeof(a)
+ * Return: stuff
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	n = n - 1;
+	while (i <= n)
+	{
+		temp = a[i];
+		a[i++] = a[n];
+		a[n--] = temp;
+	}
+}
