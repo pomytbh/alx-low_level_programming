258a765ccfa9981376f89699a768fc2143a4d26b
pomytbh <pomytbh@gmail.com>
2022-09-28 11:36:48 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - returns the length of a string
+ *@s: str
+ * Return: len(str)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(++s));
+}
